dc3e10fbe366a9eb9fe33023f13ec9c77bc197e7
okukun <lordpuk@gmail.com>
2013-10-10 11:45:09 +0200

    upload

---
+++ js/cb.js
@@ -0,0 +1,14 @@
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
+$(function() {
+	$('.carousel').carousel({
+  	interval: 5000
+})
+})
