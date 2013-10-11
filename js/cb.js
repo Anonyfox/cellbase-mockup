805f0bfc05d8f4c622e77ae0593096e02bb99425
Maximilian Stroh <Hisako1337@googlemail.com>
2013-10-11 16:32:40 +0200

    Revert 14a7907..855d113
    This rolls back to commit 14a790739ab587e2fd15e56d5b207525f62c07b9.

--- js/cb.js
+++ js/cb.js
@@ -3,9 +3,12 @@ $(function() {
   $('.dropdown-toggle').dropdown();
  
   // Fix input element click problem
-  $('.dropdown input, .dropdown label, .dropdown .container').click(function(e) {
+  $('.dropdown input, .dropdown label').click(function(e) {
     e.stopPropagation();
   });
 });
-
+$(function() {
+	$('.carousel').carousel({
+  	interval: 5000
+})
 })
