14a790739ab587e2fd15e56d5b207525f62c07b9
okukun <lordpuk@gmail.com>
2013-10-11 16:06:18 +0200

    new css classes, Cells table & submenu bar added

--- js/cb.js
+++ js/cb.js
@@ -3,12 +3,9 @@ $(function() {
   $('.dropdown-toggle').dropdown();
  
   // Fix input element click problem
-  $('.dropdown input, .dropdown label').click(function(e) {
+  $('.dropdown input, .dropdown label, .dropdown .container').click(function(e) {
     e.stopPropagation();
   });
 });
-$(function() {
-	$('.carousel').carousel({
-  	interval: 5000
-})
+
 })
